19cc0cd6b29f45e9999b099693f742c2260767d8
Nhu Chu <nnc41@scarletmail.rutgers.edu>
2023-12-28 16:34:53 -0500

    contain duplicates

---
+++ Arrays & Hashing/containDuplicate.py
@@ -0,0 +1,24 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        # Create an empty hash set to store unique elements
+        hashset = set()
+
+        # Loop through the elements in the input list 'nums'
+        for num in nums:
+            if num in hashset:
+                return True
+            else:
+                hashset.add(num)
+        return False
+      
+# Create an instance of the Solution class
+solution_instance = Solution()
+
+# Example list of integers
+nums_list = [1, 2, 3, 4, 1]  # This list contains a duplicate (1)
+
+# Call the containsDuplicate method using the instance
+result = solution_instance.containsDuplicate(nums_list)
+
+# Display the result
+print("Does the list contain duplicates?", result)
