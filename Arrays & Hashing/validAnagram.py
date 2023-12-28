bc3814e80294cf8334ecc2d570c5bacd549ccbe2
Nhu Chu <nnc41@scarletmail.rutgers.edu>
2023-12-28 17:29:53 -0500

    update

--- Arrays & Hashing/validAnagram.py
+++ Arrays & Hashing/validAnagram.py
@@ -3,24 +3,29 @@ class Solution:
     if len(s) != len(t):
       return False
     
+    # create HASHMAP to store characters and the frequency 
+    # of each character in each word
     countS, countT = {}, {}
     
     for i in range (len(s)):
       countS[s[i]] = 1 + countS.get(s[i], 0)
       countT[t[i]] = 1 + countT.get(t[i], 0)
-      
+    
+    # loop through each character of one word and compare 
+    # its frequency to that character of another word 
     for c in countS:
       if countS[c] != countT.get(c, 0):
         return False
       
     return True
-  
+
+# call class and function 
 solution_instance = Solution()
 s  = 'cat'
-t = 'tat'
-a = 'abcde'
-b = 'edcba'
-result = solution_instance.isAnagram(s, t)
+t = 'tac'
+a = 'batman'
+b = 'patman'
+result = solution_instance.isAnagram(a, b)
 print("Are the two words anagrams of each other?", result)
   
   
